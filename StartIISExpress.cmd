2682cd64593ceab43d99462dda264e5e99a8977b
Triplesticks <Triplesticks62@gmail.com>
2017-07-14 09:24:10 -0400

    Update to Bootstrap and jQuery CDN

--- StartIISExpress.cmd
+++ StartIISExpress.cmd
@@ -1,3 +1,5 @@
+REM IIS Configuration file can be found here: C:\Users\wbw07.SEAGULL\Documents\IISExpress\config
 c:
 cd "\Program Files (x86)\IIS Express"
 iisexpress /site:LearningBootstrap
+cd "\Source\Web\LearningBootstrap"
